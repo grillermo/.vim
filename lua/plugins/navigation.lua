bb1d20c65d6c7fc2726e3d9968f1945bbb6788b3
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-05-20 23:10:04 -0600

    Use fzf for shorter matches first

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -3,7 +3,10 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     config = function()
       local telescope = require('telescope')
       local actions = require('telescope.actions')
@@ -24,7 +27,17 @@ return {
             hidden = true,
           },
         },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = 'smart_case',
+          },
+        },
       }
+
+      telescope.load_extension('fzf')
     end,
   },
 
