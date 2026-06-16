452992f6cb75e13871914d734cb2a53875d04dfe
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-06-16 13:09:10 -0600

    Remove LSP hover mapping from K

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -38,7 +38,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = args.buf }
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
